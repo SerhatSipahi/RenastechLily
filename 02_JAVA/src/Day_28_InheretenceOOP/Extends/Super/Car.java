130876079ff6a5fed5018bbc079d6fd49461db42
zyilmaz02 <zyylmz24@gmail.com>
2022-08-17 20:51:57 -0500

    Day 28

---
+++ 02_JAVA/src/Day_28_InheretenceOOP/Extends/Super/Car.java
@@ -0,0 +1,26 @@
+package Day_28_InheretenceOOP.Extends.Super;
+
+public class Car {
+
+    public String Model;
+    public String Make;
+    public int year;
+
+    public Car(String model,String make,int year){
+        this.Model = model;
+        this.Make = make;
+        this.year = year;
+    }
+
+    public void Drive(){
+        System.out.println(Model +" Drives");
+    }
+    public void Speed(){
+        System.out.println(Model+" speed up");
+    }
+
+    public String display(){
+        return "Model :"+Model+ " Make: "+Make+ " Year: "+year;
+    }
+
+}
---
+++ 02_JAVA/src/Day_28_InheretenceOOP/Extends/Super/TestClass.java
@@ -0,0 +1,25 @@
+package Day_28_InheretenceOOP.Extends.Super;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("I8","BMW",2019);
+
+        car1.display();
+        car1.Drive();
+        car1.Speed();
+        System.out.println("++++++++++++++++++++++++++");
+
+        BMW bm1 = new BMW("BMW","M4",2022,4.2);
+        //bm1.display();
+        bm1.Drive();
+        bm1.Speed();
+        bm1.Fly();
+        bm1.Print();
+
+    }
+
+
+
+
+}
